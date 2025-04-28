7ea3838cede8cf77c55a18689ef5ddafeb1c46b1
jackgedge <117417072+jackgedge@users.noreply.github.com>
2025-04-28 19:56:00 +0100

    Playing with data

--- msc_code/scripts/helpers.py
+++ msc_code/scripts/helpers.py
@@ -14,7 +14,8 @@ exclusion_criteria = {
 8:	"Ingestions undertaken in controlled environment as part of voluntary study.",
 9:	"Ingestions not explicitly stated to be intentional and history not suggestive of deliberate ingestion (i.e. Age < 8, no history of previous ingestions, no psychiatric co-morbidities, not a prisoner/detainee/vulnerable group).",
 10:	"Does not meet inclusion criteria.",
-11: "Ingestions where death resulted from other means (i.e. suicide)"
+11: "Ingestions where death resulted from other means (i.e. suicide)",
+12: "Studies before the advent of Endoscopy (1906)"
 }
 
 exclusion_criteria_short = {
@@ -28,7 +29,8 @@ exclusion_criteria_short = {
 8:	"Ingested in controlled study.",
 9:	"Not explicitly intentional ingestions.",
 10:	"Inclusion criteria not met.",
-11: "Death occured from by other means (i.e. suicide)."
+11: "Death occured from by other means (i.e. suicide).",
+12: "Pre-1906."
 }
 
 def track_duplicate_removal(stage, end_count, start_count=None, tracking_dict=None):
